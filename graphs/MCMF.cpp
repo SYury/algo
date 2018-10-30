2ff2bded11306c29ddf5eddb08ab3414a4e6a26c
SYury <yusemru@gmail.com>
2018-10-30 19:51:52 +0300

    Rewrote MCMF

--- graphs/MCMF.cpp
+++ graphs/MCMF.cpp
@@ -1,44 +1,42 @@
+const long long flow_inf = 1e18;
 struct MCMF{
-	const long long flow_inf = 1e18;
 	struct FlowEdge{
 		int v, u;
-		long long cost;
-		long long flow = 0;
-		long long cap;
-		FlowEdge(){}
-		FlowEdge(int sv, int su, long long scost, long long scap):v(sv),u(su),cost(scost),cap(scap){}
+		long long cap, cost, flow = 0;
+		FlowEdge(int _v, int _u, long long _cap, long long _cost):v(_v), u(_u), cap(_cap), cost(_cost){}
 	};
-	int n;
-	vector<vector<FlowEdge> > gr;
+	vector<FlowEdge> edges;
+	vector<vector<int> > adj;
+	int n, m = 0;
+	int s, t;
 	vector<long long> pot;
 	vector<int> p;
 	queue<int> fb_q;
 	vector<int> fb_inq;
 	vector<long long> dist;
-	set<pair<long long, int> > s;
-	long long total_flow = 0;
-	long long total_cost = 0;
-	void set_n(int sn){
-		n = sn;
-		gr.resize(n);
-		pot.resize(n);
-		p.resize(n);
-		dist.resize(n);
-		fb_inq.resize(n);
-	}
-	void add_edge(int from, int to, long long cost, long long cap){
-		gr[from].push_back(FlowEdge(from, to, cost, cap));
+	set<pair<long long, int> > ds;
+	int qh, qt;
+	void resize(int _n){n = _n; adj.resize(n); pot.resize(n); p.resize(n); dist.resize(n); fb_inq.resize(n);}
+	MCMF(){}
+	MCMF(int _n, int _s, int _t){resize(_n); s = _s; t = _t;}
+	void add_edge(int v, int u, long long cap, long long cost){
+		edges.push_back(FlowEdge(v, u, cap, cost));
+		edges.push_back(FlowEdge(u, v, 0, -cost));
+		adj[v].push_back(m);
+		adj[u].push_back(m + 1);
+		m += 2;
 	}
 	void ford_bellman(){
 		pot.assign(n, flow_inf);
-		pot[0] = 0;
+		pot[s] = 0;
 		fb_inq.assign(n, false);
-		fb_inq[0] = true; fb_q.push(0);
+		fb_inq[s] = true; fb_q.push(s);
 		while(!fb_q.empty()){
 			int v = fb_q.front();
 			fb_q.pop();
 			fb_inq[v] = false;
-			for(auto e : gr[v]){
+			for(int id : adj[v]){
+				auto& e = edges[id];
 				if(e.cap - e.flow == 0)continue;
 				if(pot[e.u] > pot[v] + e.cost){
 					pot[e.u] = pot[v] + e.cost;
@@ -50,66 +48,55 @@ struct MCMF{
 	void dijkstra(){
 		dist.assign(n, flow_inf);
 		p.assign(n, -1);
-		dist[0] = 0;
-		s.insert(make_pair(0, 0));
-		while(!s.empty()){
-			int v = s.begin()->second;
-			s.erase(s.begin());
-			for(auto e : gr[v]){
+		dist[s] = 0;
+		ds.insert(make_pair(s, 0));
+		while(!ds.empty()){
+			int v = ds.begin()->second;
+			ds.erase(ds.begin());
+			for(int id : adj[v]){
+				auto& e = edges[id];
 				if(e.cap - e.flow == 0)continue;
 				long long w = pot[e.u] - pot[v] + e.cost;
 				if(dist[e.u] <= dist[v] + w)continue;
-				p[e.u] = v;
-				s.erase(make_pair(dist[e.u], e.u));
+				p[e.u] = id;
+				ds.erase(make_pair(dist[e.u], e.u));
 				dist[e.u] = dist[v] + w;
-				s.insert(make_pair(dist[e.u], e.u));
+				ds.insert(make_pair(dist[e.u], e.u));
 			}
 		}
-		for(int i = 1; i < n; i++)if(p[i] != -1)dist[i] = dist[i] - pot[i] + pot[0];
+		for(int i = 0; i < n; i++)if(i != s && p[i] != -1)dist[i] = dist[i] - pot[i] + pot[s];
 		pot.swap(dist);
 	}
-	long long min_cost_flow(long long val, bool need_first_potential = true){
-		if(need_first_potential)
-			ford_bellman();
-		for(int i = 0; i < val; i++){
+	long long min_cost_no_potential(long long val = flow_inf){
+		long long total_flow = 0, total_cost = 0;
+		for(;;){
 			dijkstra();
-			if(pot[n - 1] == flow_inf)return -flow_inf;
-			++total_flow;
-			int v = n - 1;
+			if(pot[t] == flow_inf)break;
+			int v = t;
+			long long pushed = val - total_flow;
+			if(!pushed)
+				break;
 			while(true){
-				int pv = p[v];
-				if(pv == -1)break;
-				for(auto & e : gr[pv])if(e.u == v){e.flow++; if(e.cap != 0)total_cost += e.cost;}
-				for(auto & e : gr[v])if(e.u == pv){e.flow--; if(e.cap != 0)total_cost -= e.cost;}
-				v = pv;
+				int id = p[v];
+				if(id == -1)break;
+				pushed = min(pushed, edges[id].cap - edges[id].flow);
+				v = edges[id].v;
+			}
+			total_flow += pushed;
+			v = t;
+			while(true){
+				int id = p[v];
+				if(id == -1)break;
+				total_cost += edges[id].cost * pushed;
+				edges[id].flow += pushed;
+				edges[id^1].flow -= pushed;
+				v = edges[id].v;
 			}
 		}
 		return total_cost;
 	}
-	long long min_cost_max_flow(bool need_first_potential = true){
-		if(need_first_potential)
-			ford_bellman();
-		for(;;){
-			dijkstra();
-			if(pot[n - 1] == flow_inf)break;
-			int v = n - 1;
-			long long pushed = flow_inf;
-			while(true){
-				int pv = p[v];
-				if(pv == -1)break;
-				for(auto & e : gr[pv])if(e.u == v)pushed = min(pushed, e.cap - e.flow);
-				v = pv;
-			}
-			total_flow += pushed;
-			v = n - 1;
-			while(true){
-				int pv = p[v];
-				if(pv == -1)break;
-				for(auto & e : gr[pv])if(e.u == v){e.flow += pushed; if(e.cap != 0)total_cost += e.cost * pushed;}
-				for(auto & e : gr[v])if(e.u == pv){e.flow -= pushed; if(e.cap != 0)total_cost -= e.cost * pushed;}
-				v = pv;
-			}
-		}
-		return total_cost;
+	long long min_cost(long long val = flow_inf){
+		ford_bellman();
+		return min_cost_no_potential(val);
 	}
 };
