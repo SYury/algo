678283ddd4107bd9029fda20d71367e9e0a8b9bf
SYury <yusemru@gmail.com>
2019-08-13 20:30:18 +0300

    fixed promotion

--- other/promotion.cpp
+++ other/promotion.cpp
@@ -1,20 +1,20 @@
 template<typename T>
 struct Promotion{};
 
-template<> struct Promotion<char>{int16_t example;};
-template<> struct Promotion<int8_t>{int16_t example;};
-template<> struct Promotion<int16_t>{int32_t example;};
-template<> struct Promotion<int32_t>{int64_t example;};
+template<> struct Promotion<char>{typedef int16_t promoted;};
+template<> struct Promotion<int8_t>{typedef int16_t promoted;};
+template<> struct Promotion<int16_t>{typedef int32_t promoted;};
+template<> struct Promotion<int32_t>{typedef int64_t promoted;};
 #if defined(_WIN64) || defined(__LP64__)
-template<> struct Promotion<int64_t>{__int128 example;};
+template<> struct Promotion<int64_t>{typedef __int128 promoted;};
 #endif
-template<> struct Promotion<uint8_t>{uint16_t example;};
-template<> struct Promotion<uint16_t>{uint32_t example;};
-template<> struct Promotion<uint32_t>{uint64_t example;};
+template<> struct Promotion<uint8_t>{typedef uint16_t promoted;};
+template<> struct Promotion<uint16_t>{typedef uint32_t promoted;};
+template<> struct Promotion<uint32_t>{typedef uint64_t promoted;};
 #if defined(_WIN64) || defined(__LP64__)
-template<> struct Promotion<uint64_t>{unsigned __int128 example;};
+template<> struct Promotion<uint64_t>{typedef unsigned __int128 promoted;};
 #endif
-template<> struct Promotion<float>{double example;};
-template<> struct Promotion<double>{long double example;};
+template<> struct Promotion<float>{typedef double promoted;};
+template<> struct Promotion<double>{typedef long double promoted;};
 
-#define PROM(x) (decltype(Promotion<decltype(x)>::example)(x))
+#define PROM(x) ((typename Promotion<typename std::__decay_and_strip<decltype(x)>::__type>::promoted)(x))
